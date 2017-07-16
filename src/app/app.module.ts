db021a0d890f522e24a808a3e10e85c33bf46c0b
Andre Gärtner <andregartner@Andres-MBP.home>
2017-07-16 16:39:09 +0200

    edit header and create product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,12 @@ import { AppComponent } from './app.component';
 import { routingComponents, appRouting } from './app.routing';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { ShopComponent } from './shop/shop.component';
 import { CartComponent } from './cart/cart.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContactComponent } from './contact/contact.component';
-
-
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +22,8 @@ import { ContactComponent } from './contact/contact.component';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { ShopComponent } from './shop/shop.component';
 import { CartComponent } from './cart/cart.component';
-
-
 
 export const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'shop', component: ShopComponent },
   { path: 'cart', component: CartComponent }
 ];
@@ -19,6 +19,9 @@ export const appRouting = RouterModule.forRoot(
 );
 
 export const routingComponents = [
+  HomeComponent,
+  AboutComponent,
+  ContactComponent,
   ShopComponent,
   CartComponent
 ];
