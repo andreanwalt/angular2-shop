0a727974d63e1bd9c57d2c104b0aeb38d5856d03
Andre Gärtner <andregartner@Andres-MBP.home>
2017-07-16 14:11:12 +0200

    add routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
-
+import { ShopComponent } from './shop/shop.component';
+import { CartComponent } from './cart/cart.component';
 
 export const appRoutes: Routes = [
-  //{ path: 'shop', component: ShopComponent },
-  //{ path: 'cart', component: CartComponent }
+  { path: 'shop', component: ShopComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 export const appRouting = RouterModule.forRoot(
@@ -12,5 +13,6 @@ export const appRouting = RouterModule.forRoot(
 );
 
 export const routingComponents = [
-  //ShopComponent
+  ShopComponent,
+  CartComponent
 ];
