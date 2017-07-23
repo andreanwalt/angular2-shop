cf9dbeee648ea8db797be17b3a1d6d1b5660ffb6
Andre Gärtner <andregartner@Andres-MBP.home>
2017-07-23 15:21:10 +0200

    create contact form validator and error messages

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Contact } from './contact';
 
 @Component({
   selector: 'ag-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.sass']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent{
 
-  constructor() { }
+  titles: Array<string> = ['', 'Herr', 'Frau', 'Dr'];
+  model: Object = {};
 
-  ngOnInit() {
+  constructor(){
+    this.model = new Contact( '', this.titles[0], '');
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
+
+  // send form
+  onSubmit() {
+    alert('send email');
   }
 
 }
---
+++ src/app/contact/contact.ts
@@ -0,0 +1,9 @@
+export class Contact {
+
+  constructor(
+    public title: string,
+    public firstname?: string,
+    public lastname?: string
+  ) {  }
+
+}
